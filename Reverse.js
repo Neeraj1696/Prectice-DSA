3552c5f9ac55cec92205e752f06e5805b980e852
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-07-07 23:19:21 +0530

    reverce array

--- Reverse.js
+++ Reverse.js
@@ -1,5 +1,5 @@
 let rev = 0;
-let num = 123456;
+let num = 654321;
 
 rev = Number(String(num).split("").reverse().join(""));
 
---
+++ prective.js
@@ -0,0 +1,6 @@
+let rev = 0;
+let array = [6, 5, 4, 3, 2, 1];
+
+rev = Array(String(array).split("").reverse().join(""));
+
+console.log("Reverse number : " + rev);
